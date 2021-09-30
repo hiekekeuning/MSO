26ed00de03569ec298aecf979fc12cdcf98e47f0
Hieke Keuning <12240799+hiekekeuning@users.noreply.github.com>
2021-09-30 11:09:51 +0200

    uitwerking strategy-ex

---
+++ w4-design-patterns-1/Uitwerkingen/ISignatureFormatter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signatures
+{
+    interface ISignatureFormatter
+    {
+        public abstract string Format(Account account);
+    }
+
+    class FormalSignatureFormatter : ISignatureFormatter
+    {
+        public string Format(Account account)
+        {
+            StringBuilder sig = new();
+            string line = new ('-', 60);
+            sig.AppendLine(line);
+            sig.AppendLine(account.LastName);
+            sig.AppendLine($"{account.Company}, mail {account.EMail}, tel {account.Phone}");
+            sig.AppendLine(line);
+            return sig.ToString();
+        }
+    }
+
+    class InFormalSignatureFormatter : ISignatureFormatter
+    {
+        public string Format(Account account)
+        {
+            return $"Mvg {account.FirstName}";
+        }
+    }
+
+    class EnglishSignatureFormatter : ISignatureFormatter
+    {
+        public string Format(Account account)
+        {
+            StringBuilder sig = new();
+            string line = new ('-', 60);
+            sig.AppendLine(line);
+            sig.AppendLine(account.LastName);
+            sig.AppendLine($"{account.Company}, mail {account.EMail}, phone +31 {account.Phone.Substring(1)}");
+            sig.AppendLine(line);
+            return sig.ToString();
+        }
+    }
+}
