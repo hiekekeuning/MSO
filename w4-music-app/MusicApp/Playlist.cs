b4f43048f1f1314c3c7d42c85891f8466368d622
Hieke Keuning <12240799+hiekekeuning@users.noreply.github.com>
2022-09-27 11:07:35 +0200

    files

---
+++ w4-music-app/MusicApp/Playlist.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp
+{
+    public class Playlist
+    {
+        private readonly List<Song> list = new();
+        
+        public void AddSong(Song s) 
+        { 
+            list.Add(s); 
+        }
+
+        public void Start(RecommenderMode recMode)
+        {
+            Console.WriteLine("*Playlist started*");
+
+            foreach (Song s in list)
+            {
+                s.Play();
+            }
+
+            var rec = new SongRecommender(SongDB.LoadSongDB());
+            Song recSong;
+            switch (recMode)
+            {
+                case RecommenderMode.Random:
+                    recSong = rec.SuggestRandom(list);
+                    break;
+                case RecommenderMode.Wildcard:
+                    recSong = rec.SuggestWildcard(list);
+                    break;
+                case RecommenderMode.KnownArtist:
+                    recSong = rec.SuggestFromKnownArtist(list);
+                    break;
+                default:
+                    recSong = rec.SuggestRandom(list);
+                    break;
+            }
+            Console.WriteLine("*Playlist finished*");
+            recSong.Play();
+        }
+
+    }
+}
+
---
+++ w4-music-app/MusicApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
+namespace MusicApp
+{
+    public class Program
+    {
+        
+        public static async Task Main()
+        {
+            // load DB
+            var songDB = new List<Song>(SongDB.LoadSongDB());
+
+            // Create playlist
+            var p = new Playlist();
+            p.AddSong(new Song("Crazy what love can do", new Artist("David Guetta"), Genre.Pop));
+            p.AddSong(new Song("As it was", new Artist("Harry Styles"), Genre.Pop));
+            p.AddSong(new Song("Sonne", new Artist("Rammstein"), Genre.Metal));
+            p.AddSong(new Song("Words", new Artist("Alesso"), Genre.Dance));
+            p.AddSong(new Song("Snap", new Artist("Rosa Linn"), Genre.Rock));
+
+            // Load preferences
+            var recommendMode = RecommenderMode.KnownArtist;
+            
+            // Start playlist
+            p.Start(recommendMode);
+
+            //await Spotify.SearchCodeArtists();
+            //await Spotify.SearchPatternTitles();
+        }
+   
+
+    
+    }
+
+    
+
+    
+  
+
+    
+
+    
+
+}
+
