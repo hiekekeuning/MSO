345dee3cfd5c2bbd00d66c2ef526590fed70f4b0
hiekekeuning <12240799+hiekekeuning@users.noreply.github.com>
2022-10-03 14:52:05 +0200

    added solution for QF

---
+++ w4-design-patterns-uitw/Uitwerkingen/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signatures
+{
+    class Account
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Company { get; set; }
+        public string EMail { get; set; }
+
+        public ISignatureFormatter DefaultSig { get; set; }
+
+        public string MakeSignature()
+        {
+            if (DefaultSig != null)
+            {
+                return DefaultSig.Format(this);
+            }
+            else throw new Exception("Unknown signature format");
+        }
+    }
+}
---
+++ w4-design-patterns-uitw/Uitwerkingen/Program.cs
@@ -0,0 +1,18 @@
+﻿using Signatures;
+using System;
+
+Account acc1 = new()
+{
+    FirstName = "Rianne",
+    LastName = "Jansen",
+    Company = "AlbertH",
+    EMail = "rianne@alberth.nl",
+    Phone = "0612345678",
+};
+acc1.DefaultSig = new FormalSignatureFormatter();
+Console.WriteLine(acc1.MakeSignature());
+acc1.DefaultSig = new InFormalSignatureFormatter();
+Console.WriteLine(acc1.MakeSignature());
+acc1.DefaultSig = new EnglishSignatureFormatter();
+Console.WriteLine(acc1.MakeSignature());
+
